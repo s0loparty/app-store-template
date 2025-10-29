e13ff39806f15473996b487b964d40b0eea2b56f
Alexander S. <87995029+s0loparty@users.noreply.github.com>
2025-10-29 12:04:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: '/app-store-template/',
   plugins: [
     tailwindcss(),
   ],
